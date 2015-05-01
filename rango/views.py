3968e4c54bd7483ffa896bcf8226f20fa4307641
dslowik44 <dslowik500@gmail.com>
2015-05-01 15:03:33 -0400

    Finished sec 11

--- rango/views.py
+++ rango/views.py
@@ -4,13 +4,38 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
+from datetime import datetime
 
-def index(request):  
+def index(request): 
    category_list = Category.objects.order_by('-likes')[:5]
-   context_dict = {'categories': category_list}
    top_pages_list = Page.objects.order_by('-views')[:5]
-   context_dict['top_pages'] = top_pages_list
-   return render(request, 'rango/index.html', context_dict)
+   context_dict = {'categories': category_list, 'top_pages': top_pages_list}
+  
+   visits = request.session.get('visits', 0)
+   reset_last_visit_time = False
+      
+   last_visit = request.session.get('last_visit')
+   if last_visit:
+      last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
+      if (datetime.now() - last_visit_time).days > 0:
+          visits = visits + 1
+          reset_last_visit_time = True
+   else:
+      visits = visits + 1
+      reset_last_visit_time = True
+	  
+   if reset_last_visit_time:
+      request.session['last_visit'] = str(datetime.now())
+      request.session['visits'] = visits
+   
+   context_dict['visits'] = visits
+   response = render(request, 'rango/index.html', context_dict)
+   return response
+
+def about(request):
+    context_dict = {'boldmessage': "here is the about page."}
+    context_dict['visits'] = request.session.get('visits', 0)
+    return render(request, 'rango/about.html', context_dict)
 	
 def category(request, category_name_slug):
     context_dict = {}	
@@ -120,8 +145,5 @@ def user_logout(request):
     return HttpResponseRedirect('/rango/')
 
 
-def about(request):
-    context_dict = {'boldmessage': "here is the about page."}
-    return render(request, 'rango/about.html', context_dict)
 
 
